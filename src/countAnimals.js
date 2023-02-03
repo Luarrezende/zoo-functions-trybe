5c8624003bd693dff7de68c46047ec85fe67fcd7
Luar <luarrezende@gmail.com>
2023-02-03 19:09:04 -0300

    finaliza requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    const animals = {};
+    data.species.forEach((specie) => {
+      animals[`${specie.name}`] = specie.residents.length;
+    });
+    return animals;
+  }
+  const info = data.species.find((specie) => specie.name === animal.species);
+  const sexo = info.residents.filter((sex) => sex.sex === animal.sex);
+  if (!animal.sex) {
+    return info.residents.length;
+  }
+  return sexo.length;
 };
 
+// console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
