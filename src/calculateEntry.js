157bb9620049e498de654799cc4b734e3208f80f
Luar <luarrezende@gmail.com>
2023-02-08 14:35:49 -0300

    resolve requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  if (entrants.length === undefined) {
+    return 0;
+  }
+  return {
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  };
 };
-
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const numberGuys = Object.keys(countEntrants(entrants));
+  const object = countEntrants(entrants);
+
+  return Number(numberGuys
+    .reduce((acc, entrant) => acc + (data.prices[entrant] * object[entrant]), 0)
+    .toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
