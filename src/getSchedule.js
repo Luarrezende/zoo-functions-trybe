9eda886859553fad3cbbad5511a0712d11ec8211
Luar <luarrezende@gmail.com>
2023-02-09 22:51:10 -0300

    finaliza requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
+const daysWeek = Object
+  .keys(data.hours);
+
+const zoo = () => {
+  const element = daysWeek.reduce((acc, elemento) => {
+    acc[elemento] = {
+      officeHour: `Open from ${data.hours[elemento].open}am until ${data.hours[elemento].close}pm`,
+      exhibition: data.species
+        .filter((animal) => animal.availability
+          .includes(elemento))
+        .map((a) => a.name),
+    };
+    return acc;
+  }, {});
+  element.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return element;
+};
+const semP = undefined;
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (scheduleTarget === semP) {
+    return zoo();
+  }
+  const verifica = data.species
+    .some((cond) => scheduleTarget === cond.name);
+  if (!verifica && !daysWeek.includes(scheduleTarget)) {
+    return zoo();
+  }
+  if (daysWeek.includes(scheduleTarget)) {
+    return { [scheduleTarget]: zoo()[scheduleTarget] };
+  }
+  return data.species
+    .find((teste) => teste.name === scheduleTarget).availability;
 };
 
 module.exports = getSchedule;
